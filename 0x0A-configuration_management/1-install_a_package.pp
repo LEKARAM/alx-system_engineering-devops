ad5b4a0dbc63c6214fbc7a12589d11799de9d1d8
LEKARAM <skankaleks93@gmail.com>
2023-04-27 18:32:28 +0300

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install puppet-lint
-
-python::pip { 'flask':
-  ensure   => '2.1.0',
-  pip_path => 'pip3',
-}
+# install flask from pip3
+ package { 'flask' :
+     ensure   => '2.1.0',
+         provider => 'pip3',
+             }
