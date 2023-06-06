9d095b640c449fa878c3680e9d6147c5ce59da40
LEKARAM <skankaleks93@gmail.com>
2023-06-06 18:58:12 +0300

    changes on 100-count.py

--- 0x16-api_advanced/tests/100-count.py
+++ 0x16-api_advanced/tests/100-count.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-def count_words(sentence):
+def count_words(sentence, words_to_count):
     words = sentence.split()
     word_count = {}
 
     for word in words:
-        if word in word_count:
-            word_count[word] += 1
-        else:
-            word_count[word] = 1
+        if word in words_to_count:
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
 
     return word_count
